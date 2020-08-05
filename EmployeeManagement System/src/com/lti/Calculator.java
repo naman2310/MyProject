69b29413e3fc2e8eea634272fcbf36f899c91823
naman <naman@localhost>
2020-08-05 15:49:25 +0530

    created calculator

---
+++ EmployeeManagement System/src/com/lti/Calculator.java
@@ -0,0 +1,15 @@
+package com.lti;
+
+public class Calculator {
+	
+	public static void add(int a,int b) {
+		System.out.println("sum= "+(a+b));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(10,30);
+
+	}
+
+}
