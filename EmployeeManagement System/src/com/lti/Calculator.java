bf3adbddc8250c7408ddffae376c476651901c4d
naman <naman@localhost>
2020-08-06 09:47:11 +0530

    added diff

--- EmployeeManagement System/src/com/lti/Calculator.java
+++ EmployeeManagement System/src/com/lti/Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
 	public static void add(int a,int b) {
 		System.out.println("sum= "+(a+b));
 	}
+	
+	public static void diff(int a,int b) {
+		System.out.println(a-b);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
